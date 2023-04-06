4f089e6f6eeeb0194689e8f84c1aa4941e2e627d
Keval Dhanani <dhanani.kev01@gmail.com>
2023-04-06 15:41:35 +0530

    Message by HOD data added to metadata file

--- src/metadata/aboutDeptData.js
+++ src/metadata/aboutDeptData.js
@@ -1,5 +1,18 @@
 export const data = {
-    "message-from-hod": [""],
+    "message-from-hod": {
+        "title": "Message from HOD",
+        "name": "Dr. Dhananjay Kalbande",
+        "designation": "HOD of Computer Science and Engineering Department",
+        "message": [
+            "Dear Students",
+            "I would like to extend my warmest welcome to the Department of Computer Science and Engineering. The Department offers 2 UG programs [B.Tech CSE (Data Science), B.Tech CSE (AIML) ] and 1 PG Program [ MCA ].",
+            "We pride ourselves on being pioneers to educate tomorrow's visionaries, conducting research, and leading a broad range of initiatives that validate the transformative power of Computer Science and Engineering and Computer Applications.",
+            "Our innovative academic and research institution gives serious students like you the skills needed to launch successful careers. Through high-quality academic programs enriched with today's National Education Policy(NEP-2020) that offer a unique blend of teaching, research, activity-based learning including Seva Satva, minors etc. that strengthens your ability to solve problems, you can expand your knowledge base, work independently, think critically, and find new ways to apply Science and Engineering concepts to benefit society.",
+            "Students work alongside faculty members who are leaders in their respective disciplines, have access to labs equipped with the latest technologies, and are connected to a comprehensive University providing breadth and depth of opportunities. As a result, our students acquire critical thinking capabilities as well as practically oriented skills that are prized by prestigious companies and academic institutions of National and International Importance.",
+            "All faculty members of the Department of Computer Science and Engineering are strongly committed to supporting your success. In return, we know you will study hard, seek out and respect the opinions of faculties and find opportunities to give back to your society. The efforts are consciously planned for the overall development of students. This leads our students to become competent, innovative, socially-responsible and dynamic. We are confident that our students will emerge as assets not only to this institution but also to the entire society, and the nation at large.",
+            "I wish you all very best for the successful career ahead."
+        ]
+    },
     "about-us-carousal-images": ["Carousel1.jpeg", "Carousel2.jpeg", "Carousel3.jpeg", "Carousel4.jpeg", "Carousel5.jpeg", "Carousel6.jpeg", "Carousel7.jpeg"],
     "about-the-department": [
         "The Department of Computer Science and Engineering was established in the year 2021 with the aim of providing students with the necessary skills to excel in the rapidly evolving tech industry. The department consists of 8 faculty members, 3 technical assistants and more than 220 students working towards their Bachelors’, Masters’ and Doctoral degrees. The department attracts high quality students from all over Maharashtra and also from the country.In the recent past, we have benchmarked our curriculum with select institutions of higher learning around the world and currently we are carefully reviewing the impact of these consequent changes with a view to make our programs even more strong and competitive. The department also attracts the interaction with the IT industries to provide training to the students.",
