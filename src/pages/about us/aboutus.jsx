20ae5b09c053c2460af8155cd5f9c8c7fe18aa4c
Keval Dhanani <dhanani.kev01@gmail.com>
2023-04-06 13:25:02 +0530

    About Us Page Updates and Responsive Styles Added

--- src/pages/about us/aboutus.jsx
+++ src/pages/about us/aboutus.jsx
@@ -2,6 +2,35 @@ import styles from '../../styles/about us/aboutus.module.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
+const createCarouselItemImage = (index, options = {}) => (
+    <div key={index}>
+        <img src={`/Carousel/${index}.jpeg`} />
+    </div>
+);
+
+const baseChildren = <div>{["Carousel1", "Carousel2", "Carousel3", "Carousel4", "Carousel5", "Carousel6", "Carousel7"].map(createCarouselItemImage)}</div>;
+
+const getConfigurableProps = () => ({
+    showArrows: true,
+    showStatus: false,
+    showIndicators: true,
+    infiniteLoop: true,
+    showThumbs: false,
+    useKeyboardArrows: true,
+    autoPlay: true,
+    stopOnHover: true,
+    swipeable: true,
+    dynamicHeight: true,
+    emulateTouch: true,
+    autoFocus: false,
+    thumbWidth: 100,
+    selectedItem: 0,
+    interval: 2000,
+    transitionTime: 500,
+    swipeScrollTolerance: 5
+});
+
+export const base = () => <Carousel {...getConfigurableProps()}>{baseChildren.props.children}</Carousel>;
 
 export default function AboutUs(){
 
@@ -9,19 +38,8 @@ export default function AboutUs(){
         <div id={styles.aboutus}>
             <h2 className={styles.aboutDept}> About The Department </h2>
             <div id={styles.wrapper}>
-                <Carousel className={styles.mainSlide} autoPlay={true}>
-                    <div>
-                        <img src="./Carousel/Carousel1.png" className={styles.images} alt="" />
-                    </div>
-                    <div>
-                        <img src="./Carousel/Carousel2.png" className={styles.images} alt=""/>  
-                    </div>
-                    <div>
-                        <img src="./Carousel/Carousel4.png" className={styles.images} alt=""/>
-                    </div>
-                    <div>
-                        <img src="./Carousel/Carousel6.png" className={styles.images} alt=""/>
-                    </div>
+                <Carousel {...getConfigurableProps()}>
+                    {baseChildren.props.children}
                 </Carousel>
             </div>
             
