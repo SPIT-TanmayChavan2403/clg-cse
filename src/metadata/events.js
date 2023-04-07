971c45d63dd7b171aaeb5496cb1709655bebd7fc
Keval Dhanani <dhanani.kev01@gmail.com>
2023-04-07 14:18:52 +0530

    Expert Talks Data Added

---
+++ src/metadata/events.js
@@ -0,0 +1,14 @@
+export const eventsData = {
+    "expert-talks": [
+        {
+            date: "07-04-2023",
+            time: "3PM to 5PM",
+            banner: "/events/hmi-and-its-future-in-ui-design.png",
+            title: "HMI and its future in UI designing",
+            speakerName: "Ms. Namrata Bhorade",
+            speakerDesignation: "Software Engineer at JPMC",
+            type: "Online",
+            document: "/documents/expert-talks/HMI_ Its future in UI designing.pdf"
+        },
+    ],
+};
--- src/pages/events/expertTalks.jsx
+++ src/pages/events/expertTalks.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import styles from '../../styles/experttalks.module.css'
+import styles from '../../styles/experttalks.module.css';
+import { eventsData } from '../../metadata/events';
 
 function ExpertTalks(props){
     return(
-        <div className="outer-wrapper">
-            <div className={styles.outerWrapper}>
-                <h2 className={styles.title}> Expert Talks </h2>
-                <img src="/coming-soon.png" className={styles.comingSoonImage} />
-            </div>
+        <div className={styles.outerWrapper}>
+            <h2 className={styles.title}> Expert Talks </h2>
+            {
+                eventsData["expert-talks"] && eventsData["expert-talks"] .length <= 0 ?
+                    <img src="/coming-soon.png" className={styles.comingSoonImage} /> :
+                    <div className={styles.wrapper}>
+                        <div className={styles.container}>
+                            {
+                                eventsData["expert-talks"].map((item, index) => (
+                                    <div className={styles.card} key={index}>
+                                        <img src={item.banner} alt="" className={styles.banner} />
+                                        <div className={styles.data}>
+                                            <span className={styles.eventTitle}> An Expert Talk on <strong> {item.title} </strong> </span>
+                                            <div className={styles.speaker}>
+                                                <span className={styles.speakerName}> {item.speakerName} </span>
+                                                <span className={styles.speakerDesignation}> {item.speakerDesignation} </span>
+                                            </div>
+                                            <div className={styles.eventDate}>
+                                                <span className={styles.dateHeading}> Date:  </span>
+                                                <span className={styles.dateValue}> {item.date} </span>
+                                            </div>
+                                            <div className={styles.eventTime}>
+                                                <span className={styles.timeHeading}> Time:  </span>
+                                                <span className={styles.timeValue}> {item.time} </span>
+                                            </div>
+                                            <a href={item.document} target="_blank" className={styles.document}> View Details </a>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+            }
         </div>
     )
 }
